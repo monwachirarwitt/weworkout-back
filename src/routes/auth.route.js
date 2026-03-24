fdd057965e8c407fa9d8f0d3577bbd2c61a204c6
monwachirarwitt <monwachirarwit.rattana@gmail.com>
2026-03-24 12:39:59 +0700

    save work

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,12 +1,12 @@
 // Import necessary modules
 import express from "express";
 import { validate } from "../middleware/validate.js";
-import { register } from "../controllers/auth.controller.js";
+// import { register } from "../controllers/auth.controller.js";
 import { registerSchema } from "../validations/auth.schema.js";
 
 const router = express.Router();
 
 // POST /register route
-router.post("/register", validate(registerSchema), register);
+// router.post("/register", validate(registerSchema), register);
 
 export default router;
